c9f65497912532be912f2be06dbe4679d4c3e3d7
Erik <spykim2003@gmail.com>
2019-04-16 16:43:20 -0500

    Working on register page

--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 
@@ -9,6 +11,17 @@ def index(request):
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            password
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else: form = UserCreationForm()
     context = {'form':form}
     return render(request, 'registration/register.html', context)
