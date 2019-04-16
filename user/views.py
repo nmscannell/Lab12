2e88e2ed2c356c2afdcd19250c538956edbdf226
Erik <spykim2003@gmail.com>
2019-04-16 16:30:07 -0500

    Working on register page

--- user/views.py
+++ user/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'user/index.html')
+
+
+def register(request):
+    form = UserCreationForm()
+    context = {'form':form}
+    return render(request, 'registration/register.html', context)
